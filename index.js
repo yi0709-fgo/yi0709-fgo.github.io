902ebb18fdd8f5388401dd719b0f49bc2f8102d8
yi0709-fgo <55002891+yi0709-fgo@users.noreply.github.com>
2019-10-17 23:35:09 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ CLASSES = {
   0: 'class0',
   1: 'class1',
   2: 'class2',
-  3: 'class3',
-  4: 'class4',
+  // 3: 'class3',
+  // 4: 'class4',
 };
 
 const MODEL_PATH =
     'model.json';
 
-const IMAGE_SIZE = 192;
-const TOPK_PREDICTIONS = 5;
+const IMAGE_SIZE = 150;
+const TOPK_PREDICTIONS = 3;
 
 let my_model;
 const demo = async () => {
